09499df7fd999874f57e8d769ed50ca2d4228739
DAMILARE <Joeydre69@gmail.com>
2023-03-25 22:39:12 +0000

    Initial commit based on the ready made gitpod dev environment, created the blog app, added models and templates for the blog and home apps,

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(y1$5)sdfrkfyhk&bdh%%+0^3ez2ks&5f3q4)wy(wkny=04+ym"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
